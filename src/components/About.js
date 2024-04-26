7e3ef0fcc79ec91d366a0e3679a41be9bd24787b
Abdulaziz Abdi <amigf76@gmail.com>
2024-04-26 18:09:43 +0300

    props lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,27 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+//const isBioPassed = false;
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <Bio bio = {props.bio} />
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
+}
+
+
+function Bio (props) {
+  if (!props.bio) {
+    return null
+  }
+  else {
+    return (<p>{props.bio}</p>)
+  }
 }
 
 export default About;
